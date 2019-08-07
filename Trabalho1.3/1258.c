15646d7f7e65435e529231785b09df896c62bbec
Marcos André Azevedo de Assis <marcosandreazevedo@hotmail.com>
2019-08-07 10:27:43 -0300

    Update 1258.c

--- Trabalho1.3/1258.c
+++ Trabalho1.3/1258.c
@@ -1,45 +1,21 @@
 #include <stdio.h>
 #include <string.h>
-#include <math.h>
 #include <stdlib.h>
 
 typedef struct{
 	char nome[100];
 	char cor[10];
-	char t;
+	char tamanho;
 }turma;
 
-int Compara(const void *, const void *);
-
-int main(){
-	int n, i, j = 0;
-	turma Turma[60];
-	
-	while(scanf("%d ", &n) && n != 0){
-		for(i=0;i<n;i++){
-			fgets(Turma[i].nome, 50, stdin);
-			scanf("%s %c ", Turma[i].cor, &Turma[i].t);
-		}
-		qsort(Turma, n, sizeof(turma), Compara);
-		if(j){
-			printf("\n");
-		}else{
-			j++;
-		}
-		for(i=0;i<n;i++){
-			printf("%s %c %s\n", Turma[i].cor, Turma[i].t, Turma[i].nome);
-		}
-	}
-	return 0;
-}
-int Compara(const void *p1, const void *p2){
+int Comparacao(const void *p1, const void *p2){
 	turma *i = (turma *)p1, *j = (turma *)p2;
-	
+
 	if(strcmp(i->cor, j->cor) == 0){
-		if(i->t > j->t){
+		if(i->tamanho > j->tamanho){
 			return -1;
 		}else{
-			if(i->t < j->t){
+			if(i->tamanho < j->tamanho){
 				return 1;
 			}else{
 				return strcmp(i->nome, j->nome);
@@ -48,4 +24,25 @@ int Compara(const void *p1, const void *p2){
 	}else{
 		return strcmp(i->cor, j->cor);
 	}
+}
+
+int main(){
+	int n, i, j = 0, linha = 0;
+	turma Turma[60];
+
+	while(scanf("%d ", &n) && n != 0){
+		if(linha){
+		    printf("\n");
+		}
+		linha = 1;
+		for(i=0;i<n;i++){
+			fgets(Turma[i].nome, 50, stdin);
+			scanf("%s %c ", Turma[i].cor, &Turma[i].tamanho);
+		}
+		qsort(Turma, n, sizeof(turma), Comparacao);
+		for(i=0;i<n;i++){
+			printf("%s %c %s", Turma[i].cor, Turma[i].tamanho, Turma[i].nome);
+		}
+	}
+	return 0;
 }
